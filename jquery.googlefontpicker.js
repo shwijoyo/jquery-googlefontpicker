b1fec0e63d215359df392fbf17d9e4b67b5f9c7c
shwijoyo <hadiwijoyo.suro@gmail.com>
2023-09-17 14:53:48 +0700

    clear pagination

--- jquery.googlefontpicker.js
+++ jquery.googlefontpicker.js
@@ -12916,19 +12916,7 @@ $.fn.googlefontpicker = function (options) {
       <div class="modal-footer">
  <nav>
   <ul class="pagination pagination-sm">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
     
-    <li class="page-item"><a class="page-link">...</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
   </ul>
 </nav>
       </div>
